23378b7033e3eabfe2d513f0c19d2eb7aed38901
Tristana_Dragon_Trainer <48664307+DragonTrainerTristana@users.noreply.github.com>
2023-02-06 13:35:08 +0900

    .,.

---
+++ Assets/Scripts/SceneManagement/IntroMenu.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class IntroMenu : MonoBehaviour
+{
+    public void OnClickMenu() {
+        SceneManager.LoadScene("MenuScene");
+    }
+    public void OnClickScene1()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+    public void OnClickScene2()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+    public void OnClickScene3()
+    {
+        SceneManager.LoadScene("Scene3");
+    }
+    public void OnClickScene4()
+    {
+        SceneManager.LoadScene("Scene4");
+    }
+    public void OnClickScene5()
+    {
+        SceneManager.LoadScene("Scene5");
+    }
+
+}
